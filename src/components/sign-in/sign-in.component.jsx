e1ded226e7e3c804228da337944e507fb84ff499
unknown <you@example.com>
2021-10-28 03:19:30 +0200

    completing sign in and sign up page

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -15,18 +15,22 @@ constructor(props){
         email: '',
         password:''
     }
-}
-handleSubmit = event => {
+};
+handleSubmit = async event => {
     event.preventDefault();
-
-    this.setState({ email:'', password:'' });
-}
+     const { email, password } = this.state;
+     
+     try{
+         await auth.signInWithEmailAndPassword(email, password);
+         this.setState({ email:'', password:'' });
+     } catch (error) { console.log(error);}
+};
 handleChange = event => {
     const { value, name } = event.target; //event.target is what has been typed by the user(password value or email value)
 
     this.setState({ [name]: value });
 
-}
+};
 render() {
     return(
         <div className='sign-in'>
