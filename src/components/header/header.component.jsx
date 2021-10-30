0d830e6aafd2df6772af3db950d64a66b48544d1
unknown <you@example.com>
2021-10-30 14:09:01 +0200

    use Redirect component to redirect to homePage once a user is signed in

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -29,7 +29,7 @@ const  mapStateToProps = state => ({
   currentUser: state.user.currentUser
 })
 
-export default connect(mapStateToProps)(Header);
+export default connect(mapStateToProps)(Header); 
  
 
 
