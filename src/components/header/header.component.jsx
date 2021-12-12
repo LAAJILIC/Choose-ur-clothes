2cb9da14d5f23fbf6f2d0017e1333bec95b3b755
unknown <you@example.com>
2021-12-12 16:05:21 +0100

    updated logo for fashion website

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,13 +8,12 @@ import { selectCurrentUser } from '../../redux/user/user.selectors';
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
-import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 
 const Header = ({ currentUser, hidden }) => (
     <div className='header'>
       <Link className='logo-container' to='/'>
-        <Logo className='logo' />
+      <img className='img-logo' src='https://svgshare.com/i/ciK.svg' />
       </Link>
      <div className='options'>
        <Link className='option' to='/shop'>SHOP</Link>
